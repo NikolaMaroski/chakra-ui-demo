2a313057389a7faecf7405ebd58f0093c363a186
Nikola Maroski <nikolamaroski@hotmail.com>
2021-10-08 02:07:31 +0200

    define custom colors and fonts in Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,12 @@ import {AppProps} from 'next/app';
 import {ChakraProvider} from '@chakra-ui/react';
 import React from "react";
 
+import theme from '../src/theme/theme';
+import '../src/theme/styles.css'
+
 const App = ({Component, pageProps}: AppProps) => {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <Component {...pageProps} />
         </ChakraProvider>
     );
